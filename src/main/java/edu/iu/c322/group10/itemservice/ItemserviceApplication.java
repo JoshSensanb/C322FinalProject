cecbfd74a4bfd3520eed393806596434a67f90ea
Jack Ramos <ramos.jack525@yahoo.com>
2023-04-07 14:32:21 -0400

    test commit

---
+++ src/main/java/edu/iu/c322/group10/itemservice/ItemserviceApplication.java
@@ -0,0 +1,13 @@
+package edu.iu.c322.group10.itemservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItemserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItemserviceApplication.class, args);
+	}
+
+}
---
+++ src/main/java/edu/iu/c322/group10/itemservice/repository/ItemRepository.java
@@ -0,0 +1,7 @@
+package edu.iu.c322.group10.itemservice.repository;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class ItemRepository {
+}
