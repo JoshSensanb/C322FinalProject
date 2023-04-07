9a553bf0779feffaaaf5cd9bb05613ab301d4364
Jack Ramos <ramos.jack525@yahoo.com>
2023-04-07 14:57:56 -0400

    repo/controller/model skeleton

---
+++ src/main/java/edu/iu/c322/group10/itemservice/model/Comment.java
@@ -0,0 +1,8 @@
+package edu.iu.c322.group10.itemservice.model;
+
+public class Comment {
+    private int itemId;
+    private String customerName;
+    private String body;
+
+}
--- src/main/java/edu/iu/c322/group10/itemservice/repository/ItemRepository.java
+++ src/main/java/edu/iu/c322/group10/itemservice/repository/ItemRepository.java
@@ -1,7 +1,10 @@
 package edu.iu.c322.group10.itemservice.repository;
 
+import edu.iu.c322.group10.itemservice.model.Item;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public class ItemRepository {
+public interface ItemRepository extends JpaRepository<Item, Integer> {
+
 }
