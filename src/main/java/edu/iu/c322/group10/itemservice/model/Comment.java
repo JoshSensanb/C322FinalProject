b4f33a7b4e2a75160fe22a78aa195439935e1c1d
DarrionS <106341705+DarrionS@users.noreply.github.com>
2023-04-09 23:22:23 -0400

    Update Comment.java
    Made Comment class an entity

--- src/main/java/edu/iu/c322/group10/itemservice/model/Comment.java
+++ src/main/java/edu/iu/c322/group10/itemservice/model/Comment.java
@@ -1,6 +1,10 @@
 package edu.iu.c322.group10.itemservice.model;
 
+@Entity
 public class Comment {
+    
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
     private int itemId;
     private String customerName;
     private String body;
